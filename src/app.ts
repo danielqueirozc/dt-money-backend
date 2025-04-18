46d7b3dff6ab9362cbe68e808e2bc4f3929d4080
danielqueirozc <danielqueiroz.job@gmail.com>
2025-04-18 17:40:40 -0300

    create setup

---
+++ src/app.ts
@@ -0,0 +1,4 @@
+import fastify from "fastify";
+
+export const app = fastify({logger: true}) // logger: true => mostra os logs no terminal
+
---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import { app } from "./app";
+
+app.listen({
+    host: '0.0.0.0',	
+    port: 4000,
+})
+.then(() => {
+    console.log('🚀 HTTP server running on http://localhost:4000')
+})
