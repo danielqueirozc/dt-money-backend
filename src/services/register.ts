189ac2d1bb9dd99c2ebd5ea63f460bbc7d22c56a
danielqueirozc <danielqueiroz.job@gmail.com>
2025-04-18 20:16:51 -0300

    feat: create RegisterService

--- src/services/register.ts
+++ src/services/register.ts
@@ -1,4 +1,33 @@
+import { UsersRepository } from "@/repositories/users-repository";
+import { User } from "@prisma/client";
+import { hash } from "bcryptjs";
+
 interface RegisterRequest {
+    name: string;
     email: string;
     password: string;
+}
+
+interface RegisterResponse {
+    user: User
+}
+
+export class RegisterService {
+    constructor (private usersRepository: UsersRepository) {}
+
+    async execute({ name, email, password }: RegisterRequest): Promise<RegisterResponse> {
+        const userWithSameEmail = await this.usersRepository.findByEmail(email)
+
+        if (userWithSameEmail) {
+            throw new Error('User already exists.')
+        }
+
+        const password_hash = await hash(password, 7)
+
+        const user = await this.usersRepository.create({ name, email, password_hash })
+
+        return {
+            user
+        }
+    }
 }
