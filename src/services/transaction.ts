e92ff3baee8c925d51312af4b9d72f59f82a4c09
danielqueirozc <danielqueiroz.job@gmail.com>
2025-04-20 18:54:06 -0300

    feat: create transaction service

---
+++ src/services/transaction.ts
@@ -0,0 +1,21 @@
+import { TransactionsRepository } from "@/repositories/transactios-repository";
+import { Prisma, Transaction } from "@prisma/client";
+interface TrnsactionRequest {
+    data: Prisma.TransactionCreateInput
+}
+
+export class TransactionService {
+    constructor(private transactionRepository: TransactionsRepository) {}
+
+    async execute({ data }: TrnsactionRequest): Promise<Transaction> {
+        const transaction = await this.transactionRepository.create(data) // constructor e public se referir com this
+
+        return transaction
+    }
+
+    async getAll(): Promise<Transaction[]> {
+        const transactions = await this.transactionRepository.getAll()
+
+        return transactions
+    } 
+}
