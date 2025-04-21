f61648603f1c5304e18350423b71a276506490e9
danielqueirozc <danielqueiroz.job@gmail.com>
2025-04-21 17:41:27 -0300

    feat: create register jwt

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,17 @@
-import { PrismaClient } from "@prisma/client";
 import fastify from "fastify";
+import fastifyJwt from "fastify-jwt";
+
+import 'dotenv/config'
+import fastifyCookie from "@fastify/cookie";
 
 export const app = fastify({ logger: true }) // logger: true => mostra os logs no terminal
+
+app.register(fastifyJwt, {
+    secret: process.env.JWT_SECRET || 'secret',
+    cookie: {
+        cookieName: 'token',
+        signed: false
+    }
+})
+
+app.register(fastifyCookie)
