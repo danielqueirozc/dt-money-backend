6e0ea27148fb2e25c25031d8b4b2e183c3a69cbc
danielqueirozc <danielqueiroz.job@gmail.com>
2025-04-21 15:27:25 -0300

    test: update metod create

--- src/repositories/in-memory/in-memory-users-repository.ts
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -3,10 +3,10 @@ import { UsersRepository } from '../users-repository'
 import { randomUUID } from 'crypto'
 
 export class InMemoryUsersRepository implements UsersRepository {
-    public items: User[] = []
+    public users: User[] = []
 
     async findById(id: string): Promise<User | null> {
-        const user = this.items.find(item => item.id === id)
+        const user = this.users.find(item => item.id === id)
 
         if (!user ) {
             return null
@@ -16,7 +16,7 @@ export class InMemoryUsersRepository implements UsersRepository {
     }
 
     async findByEmail(email:string): Promise<User | null> {
-        const user = this.items.find(item => item.id === email)
+        const user = this.users.find(item => item.email === email)
 
         if (!user) {
             return null
@@ -28,13 +28,10 @@ export class InMemoryUsersRepository implements UsersRepository {
     async create(data: Prisma.UserCreateInput): Promise<User> {
         const user = {
             id: randomUUID(),
-            name: data.name,
-            email: data.email,
-            password_hash: data.password_hash,
-            created_at: new Date(),
+            ...data
         }
 
-        this.items.push(user)
+        this.users.push(user)
 
         return user
     }
