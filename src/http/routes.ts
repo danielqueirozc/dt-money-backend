ad72a646917faafbdc111aad3207a693dc456b1e
danielqueirozc <danielqueiroz.job@gmail.com>
2025-04-23 19:37:29 -0300

    fix: rename routes and delete pre handler from the route authenticate

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,11 +1,11 @@
 import { FastifyInstance } from "fastify";
-import { Register } from "./controllers/resgister";
+import { Register } from "./controllers/register";
 import { Authenticate } from "./controllers/authenticate";
 import { VerifyJwt } from "@/middlewares/verify-jwt";
 import { CreateTransaction } from "./controllers/transaction";
 
 export function AppRoutes(app: FastifyInstance) {
-    app.get('/sessios', Register),
-    app.post('athenticate',{preHandler: VerifyJwt}, Authenticate)
-    app.post('transaction', {preHandler: VerifyJwt}, CreateTransaction)
+    app.post('/sessions', Register),
+    app.post('/authenticate', Authenticate)
+    app.post('/transaction', {preHandler: VerifyJwt}, CreateTransaction)
 }
