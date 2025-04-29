d077b1296885c7c2f90b9e45999735b8ca309676
danielqueirozc <danielqueiroz.job@gmail.com>
2025-04-29 19:11:57 -0300

    refactor: changed user by userId

--- src/http/controllers/transaction.ts
+++ src/http/controllers/transaction.ts
@@ -4,32 +4,27 @@ import { z } from "zod";
 
 export async function CreateTransaction(request: FastifyRequest, reply: FastifyReply) {
     const createTransactionSchema = z.object({
-       data: z.object({
-            description: z.string(),
-            price: z.number(),
-            category: z.string(),
-            type: z.enum(['INCOME', 'OUTCOME'])
-       })
+      description: z.string(),
+      price: z.number(),
+      category: z.string(),
+      type: z.enum(['income', 'outcome']),
     })
 
-    const { data } = createTransactionSchema.parse(request.body)
+    const { description, price, category, type } = createTransactionSchema.parse(request.body)
 
     try {
         await request.jwtVerify()
 
-        const userId = (request.user as { sub: string }).sub;
+        const userId = (request.user as { sub: string }).sub
 
         const createTransactionService = MakeCreateTransactionService()
 
         await createTransactionService.execute({
-            data: {
-              ...data,
-              user: {
-                connect: {
-                  id: userId,
-                },
-              },
-            },
+            userId,
+            description,
+            price,
+            category,
+            type
           })
     } catch (error) {
         if (error instanceof Error) {
