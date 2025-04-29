93ec99e80f5b2cc34870f9081ada656b43cba9ca
danielqueirozc <danielqueiroz.job@gmail.com>
2025-04-29 19:13:21 -0300

    refactor: remove data

--- src/services/transaction.ts
+++ src/services/transaction.ts
@@ -1,14 +1,30 @@
 import { TransactionsRepository } from "@/repositories/transactios-repository";
-import { Prisma, Transaction } from "@prisma/client";
+import { Transaction } from "@prisma/client";
+import { TransactionType } from "@prisma/client";
+
 interface TransactionRequest {
-    data: Prisma.TransactionCreateInput
+    description: string
+    price: number
+    category: string
+    type: TransactionType
+    userId: string
 }
 
 export class TransactionService {
     constructor(private transactionRepository: TransactionsRepository) {}
 
-    async execute({ data }: TransactionRequest): Promise<Transaction> {
-        const transaction = await this.transactionRepository.create(data) // constructor e public se referir com this
+    async execute({ description, price, category, type, userId }: TransactionRequest): Promise<Transaction> {
+        const transaction = await this.transactionRepository.create({
+            user: {
+                connect: {
+                    id: userId
+                }
+            },
+            description,
+            price,
+            category,
+            type
+        }) // constructor e public se referir com this
 
         return transaction
     }
